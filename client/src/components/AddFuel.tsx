b16a66c11fd2b4f62bdf525f26c7dbff94feda41
Kristiyan Voynski <krisivv@gmail.com>
2024-02-11 21:41:05 +0200

    add fuel service

--- client/src/components/AddFuel.tsx
+++ client/src/components/AddFuel.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, styled, useTheme } from '@mui/material';
+import { Box, Button, styled, TextField } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import { addFuel } from '../service/FuelService.ts';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -15,33 +16,67 @@ const VisuallyHiddenInput = styled('input')({
 });
 
 function AddFuel() {
-    const theme = useTheme();
+    const [file, setFile] = useState<FileList | null>(null);
+    const [fuelType, setFuelType] = useState('');
+    const [fuelCost, setFuelCost] = useState(0);
+    const [fuelAmount, setFuelAmount] = useState(0);
 
-    const handleSubmit = (event: FormEvent) => {
+
+    const handleSubmit = async (event: FormEvent) => {
         event.preventDefault();
+        console.log(event)
 
-        const formData = new FormData(event.target as HTMLFormElement);
+        const form = new FormData();
+        form.set('fuelType', fuelType);
+        form.set('fuelCost', fuelCost.toString());
+        form.set('fuelAmount', fuelAmount.toString());
 
-        console.log(formData.get('file'));
+        if (file) {
+            form.set('file', file[0]);
+        }
+        const res = await addFuel(form);
+        console.log(res)
     }
 
     return (
-        <Box component="form"
-             onSubmit={ handleSubmit }
-             sx={ {
-                 flexGrow: 1,
-                 height: `calc(100vh - ${ theme.spacing(14) })`,
-             } }>
-
-            <Button component="label" variant="contained" startIcon={ <CloudUploadIcon/> }>
+        <Box component="form" onSubmit={ handleSubmit } noValidate sx={ { mt: 1 } }>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Fuel Type"
+                type="text"
+                onChange={ e => setFuelType(e.target.value) }
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Fuel Costs"
+                type="number"
+                onChange={ e => setFuelCost(Number(e.target.value)) }
+            />
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Fuel Amount"
+                type="number"
+                onChange={ e => setFuelAmount(Number(e.target.value)) }
+            />
+            <Button component="label" fullWidth variant="contained" startIcon={ <CloudUploadIcon/> }>
                 Upload file
-                <VisuallyHiddenInput type="file"/>
+                <VisuallyHiddenInput onChange={ event => setFile(event.target.files) } type="file"/>
             </Button>
-            <Button type="submit" variant="contained" sx={ { mt: 3, mb: 2 } }>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={ { mt: 3, mb: 2 } }
+            >
                 Save Fuel Report
             </Button>
         </Box>
-
     );
 }
 
--- client/src/service/FuelService.ts
+++ client/src/service/FuelService.ts
@@ -1,16 +1,31 @@
 const baseUrl = 'http://localhost:5000/fuel';
 
+export const addFuel = async (form: FormData) => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        body: form,
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+        }
+    });
+}
+
+export const getFuel = async () => {
+    const response = await fetch(baseUrl);
+    return response.json();
+}
+
 export const getDaily = async () => {
-    const response = await fetch(`${baseUrl}/daily`);
+    const response = await fetch(`${ baseUrl }/daily`);
     return response.json();
 }
 
 export const getYearly = async () => {
-    const response = await fetch(`${baseUrl}/yearly`);
+    const response = await fetch(`${ baseUrl }/yearly`);
     return response.json();
 }
 
 export const getMonthly = async () => {
-    const response = await fetch(`${baseUrl}/monthly`);
+    const response = await fetch(`${ baseUrl }/monthly`);
     return response.json();
 }
